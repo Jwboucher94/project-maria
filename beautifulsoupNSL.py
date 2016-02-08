562668d7932195deea9f98861ee283480212645e
Jack Boucher <butticorn@gmail.com>
2016-02-08 03:42:17 -0500

    update to suffolk
    moved cache/suffolk6578.php to a variable, to minimize errors in
    changing, and switched from networksciencelab to suffolk page 6578
    (faculty list). had to add u’\u2013’ and u’\u2019’ to the replace list.
    If dimitry ever looks at this, help with this problem would be great.
    Will be bringing it up in class. Not sure why others didn’t have
    problems with this, I got the code from the site…

--- beautifulsoupNSL.py
+++ beautifulsoupNSL.py
@@ -7,16 +7,19 @@ Created on Mon Feb  1 16:46:00 2016
 import os.path
 import bs4 as BeautifulSoup
 import urllib.request
+url6578 = 'cache/suffolk6578.php'
 
-if not os.path.isfile("cache/networksciencelab.com.html"):
+if not os.path.isfile(url6578):
     print("Attempting to cashing the data...")
     if not os.path.isdir("cache"):
         os.mkdir("cache")
-    data = urllib.request.urlopen("http://networksciencelab.com/").read().decode("utf-8")
+    data = urllib.request.urlopen("http://www.suffolk.edu/college/6578.php").read().decode("utf-8")
+    data = data.replace(u'\u2013', u'-')
     data = data.replace(u'\xa0', u' ')
-    outfile = open("cache/networksciencelab.com.html", "w")
+    data = data.replace(u'\u2019', u"\'")
+    outfile = open(url6578, "w")
     outfile.write(data)
     outfile.close()
 
-with open("cache/networksciencelab.com.html") as infile:
+with open(url6578) as infile:
     soup = BeautifulSoup.BeautifulSoup(infile, 'lxml')
