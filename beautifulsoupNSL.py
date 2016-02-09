09f841fa7c8538e3a6225b56bf21c063080292d7
Jack Boucher <butticorn@gmail.com>
2016-02-09 17:46:13 -0500

    minor changes

--- beautifulsoupNSL.py
+++ beautifulsoupNSL.py
@@ -4,43 +4,46 @@ Created on Mon Feb  1 16:46:00 2016
 
 @author: butticorn
 """
+
+#Import Statements
 import os.path
-import bs4 as BeautifulSoup     #import statements...
+import bs4 as BeautifulSoup     
 import urllib.request
 import csv
 
+#Cache and CSV file variables to minimize possible errors
 url6578 = 'cache/suffolk6578.php'
-teachersCsv = 'cache/faculty-education.csv'   #variables for files. same folder for ease.
+teachersCsv = 'cache/faculty-education.csv'   
 
 def findli(type):
-    if not temped[tempvar].get_text().find(type) == -1:
-        empty[2] = type
-        empty[3] = temped[tempvar].get_text().replace('\xa0', '').split(', ')[1].split('\n')[0]
+    if not pdegree[pvar].get_text().find(type) == -1:
+        plist[2] = type
+        plist[3] = pdegree[pvar].get_text().replace('\xa0', '').split(', ')[1].split('\n')[0]
                         
 def findp(type):
-    if not temped[tempvar].find(type) == -1:
-        empty[2] = type
-        empty[3] = temped[tempvar].replace('\xa0', '').split(', ')[1]
+    if not pdegree[pvar].find(type) == -1:
+        plist[2] = type
+        plist[3] = pdegree[pvar].replace('\xa0', '').split(', ')[1]
         
         
 if not os.path.isfile(url6578):
     print("Attempting to cashing the data...")
     if not os.path.isdir("cache"):
         os.mkdir("cache")
-    data = urllib.request.urlopen("http://www.suffolk.edu/college/6578.php").read().decode("utf-8")
-    data = data.replace(u'\u2013', u'-')
-    data = data.replace(u'\xa0', u' ')
-    data = data.replace(u'\u2019', u"\'")
+    main = urllib.request.urlopen("http://www.suffolk.edu/college/6578.php").read().decode("utf-8")
+    main = main.replace(u'\u2013', u'-')
+    main = main.replace(u'\xa0', u' ')
+    main = main.replace(u'\u2019', u"\'")
     outfile = open(url6578, "w")
-    outfile.write(data)
+    outfile.write(main)
     outfile.close()
 
 with open(url6578) as infile:
     soup = BeautifulSoup.BeautifulSoup(infile, 'lxml')
 
 modefind = soup.find('ul', { "class" : "mod-showhide" })
-depcount = len(modefind.find_all('h2'))+1 #department count
-countdep = 0
+dlen = len(modefind.find_all('h2'))+1 #department count
+dcounter = 0
 if not os.path.isfile(teachersCsv): #create the teachers.csv file, if not already created
     header = ["Name", "Rank", "Degree", "Institution"]
     print("Creating teachers.csv to store data")
@@ -58,108 +61,122 @@ else:
     
 
 #Here's where the "Fun" begins
-
-while countdep < depcount:
-    profcount = len(modefind.find_all('ul')[countdep].find_all('li'))
+dcounter = 0
+while dcounter < dlen:
+    plen = len(modefind.find_all('ul')[dcounter].find_all('li'))
         #finding out how many professors are in the current department
-    countprof = 0
+    pcounter = 0
         #resetting the current professor count for each department
-    while countprof < profcount:
-        empty = ["", "", "", ""]
+    while pcounter < plen:
+        plist = ["", "", "", ""]
         rowcount = 0
-            #splits name and rank into a tuple, used variable to slim down for next two
+            #splits pname and rank into a tuple, used variable to slim down for next two
         
-        if len(modefind.find_all('ul')[countdep].find_all('li')[countprof].find_all('a')) > 0:
+        if len(modefind.find_all('ul')[dcounter].find_all('li')[pcounter].find_all('a')) > 0:
             #if length of websites is greater than 0 (has a website)
-            empref = modefind.find_all('ul')[countdep].find_all('a')[countprof].get('href')
-            #find the href and set to empref
-            if empref.startswith('http://www.suffolk.edu'): #just in case it starts with prefix
-                empref = empref.split('http://www.suffolk.edu')[1] #get the current website
+            profhref = modefind.find_all('ul')[dcounter].find_all('a')[pcounter].get('href')
+            #find the href and set to profhref
+            if profhref.startswith('http://www.suffolk.edu'): #just in case it starts with prefix
+                profhref = profhref.split('http://www.suffolk.edu')[1] #get the current website
                         #About to get full of bullshit code right here...
-            tempdata = urllib.request.urlopen("http://www.suffolk.edu" + empref).read().decode("utf-8")
-            tempdata = tempdata.replace(u'\u2013', u'-')
-            tempdata = tempdata.replace(u'\xa0', u' ')
-            tempdata = tempdata.replace(u'\u2019', u"\'")
-                #set current web address to tempdata
-            temploc = tempdata.find('3>Education') 
+            pdata = urllib.request.urlopen("http://www.suffolk.edu" + profhref).read().decode("utf-8")
+            pdata = pdata.replace(u'\u2013', u'-')
+            pdata = pdata.replace(u'\xa0', u' ')
+            pdata = pdata.replace(u'\u2019', u"\'")
+                #set current web address to pdata
+            if pdata.find('3>Education') > 0:
+                ploc = pdata.find('3>Education')
+                pdata2 = pdata[ploc:]
+            elif pdata.find('Education\n') > 0:
+                ploc = pdata.find('Education\n')
+                pdata2 = pdata[ploc:]
+            elif pdata.find('<strong>Education') > 0:
+                ploc = pdata.find('<strong>Education')
+                pdata2 = pdata[ploc:]
+            elif pdata.find('Education</strong>') > 0:
+                ploc = pdata.find('Education</strong>')
+                pdata2 = pdata[ploc:]
             #set temp location to shorten soup            
-            tempdata2 = tempdata[temploc:]
-            tempsoup = BeautifulSoup.BeautifulSoup(tempdata[temploc:temploc+250], 'lxml')
-            if len(tempsoup.find_all('h3')) > 0:
-                temploc2 = tempdata2.find('<h3')
+                        
+            psoup = BeautifulSoup.BeautifulSoup(pdata[ploc:ploc+250], 'lxml')
+            if len(psoup.find_all('h3')) > 0:
+                ploc2 = pdata2.find('<h3')
             else:
-                if len(tempsoup.find_all('ul')) > 0:
-                    temploc2 = tempdata2.find('/ul>')+5
+                if len(psoup.find_all('ul')) > 0:
+                    ploc2 = pdata2.find('/ul>')+5
                 else:
-                    temploc2 = tempdata2.find('/p>')+4
-            tempdata2 = tempdata2[:temploc2]
-            tempsoup = BeautifulSoup.BeautifulSoup(tempdata2, 'lxml')
+                    ploc2 = pdata2.find('/p>')+4
+            pdata2 = pdata2[:ploc2]
+            psoup = BeautifulSoup.BeautifulSoup(pdata2, 'lxml')
                 #temp beautifulSoup
-            if not len(tempsoup.find_all('li')) == 0:
-                temped = tempsoup.find_all('li')
-                templi = 1
+            if not len(psoup.find_all('li')) == 0:
+                pdegree = psoup.find_all('li')
+                countli = 1
             else:
-                if temploc == -1:
-                    temped = tempsoup
-                if temploc > 0:
-                    temped = tempsoup.find_all('p')[1].get_text().split('\n ')
-                templi = 0
-            templen = len(temped)
-            tempvar = 0
+                if ploc == -1:
+                    pdegree = psoup
+                if ploc > 0:
+                    if len(psoup.find_all('p')) > 1:
+                        pdegree = psoup.find_all('p')[1].get_text().split('\n ')
+                    else:
+                        pdegree = psoup.find_all('p')[0].get_text().split('\n ')
+                countli = 0
+            lenpdegree = len(pdegree)
+            pvar = 0
                     
-            while tempvar < templen:
-                if templi == 1:
-                    name = modefind.find_all('ul')[countdep].find_all('li')[countprof].get_text().split(', ')
-                    empty[0] = name[0]
-                    empty[1] = name[1]
-                    empty[2] = ''
-                    empty[3] = ''
+            while pvar < lenpdegree:
+                if countli == 1:
+                    pname = modefind.find_all('ul')[dcounter].find_all('li')[pcounter].get_text().split(', ')
+                    plist[0] = pname[0]
+                    plist[1] = pname[1]
+                    plist[2] = ''
+                    plist[3] = ''
                     findli('PhD')
                     findli('MS')
                     findli('BS')
-                    if not empty[2] == '':
+                    if not plist[2] == '':
                         with open(teachersCsv, 'a', newline='') as csvfile:
                         #'a' to append instead of overwrite the first line each time
                             writer = csv.writer(csvfile)
-                            writer.writerow(empty)
+                            writer.writerow(plist)
                         rowcount += 1
-                    if empty[2] == '' and (tempvar+1) == templen and rowcount == 0:
+                    if plist[2] == '' and (pvar+1) == lenpdegree and rowcount == 0:
                         with open(teachersCsv, 'a', newline='') as csvfile:
                         #'a' to append instead of overwrite the first line each time
                             writer = csv.writer(csvfile)
-                            writer.writerow(empty)
-                    tempvar += 1
-                if templi == 0:
-                    name = modefind.find_all('ul')[countdep].find_all('li')[countprof].get_text().split(', ')
-                    empty[0] = name[0]
-                    empty[1] = name[1]
-                    empty[2] = ''
-                    empty[3] = ''
+                            writer.writerow(plist)
+                    pvar += 1
+                if countli == 0:
+                    pname = modefind.find_all('ul')[dcounter].find_all('li')[pcounter].get_text().split(', ')
+                    plist[0] = pname[0]
+                    plist[1] = pname[1]
+                    plist[2] = ''
+                    plist[3] = ''
                     findp('PhD')
                     findp('MS')
                     findp('BS')
-                    if not empty[2] == '':
+                    if not plist[2] == '':
                         with open(teachersCsv, 'a', newline='') as csvfile:
                         #'a' to append instead of overwrite the first line each time
                             writer = csv.writer(csvfile)
-                            writer.writerow(empty)
+                            writer.writerow(plist)
                         rowcount += 1
-                    if empty[2] == '' and (tempvar+1) == templen:
+                    if plist[2] == '' and (pvar+1) == lenpdegree:
                         with open(teachersCsv, 'a', newline='') as csvfile:
                         #'a' to append instead of overwrite the first line each time
                             writer = csv.writer(csvfile)
-                            writer.writerow(empty)
-                    tempvar += 1
+                            writer.writerow(plist)
+                    pvar += 1
         else:
-            name = modefind.find_all('ul')[countdep].find_all('li')[countprof].get_text().split(', ')
-            empty[0] = name[0]
-            empty[1] = name[1]
+            pname = modefind.find_all('ul')[dcounter].find_all('li')[pcounter].get_text().split(', ')
+            plist[0] = pname[0]
+            plist[1] = pname[1]
             with open(teachersCsv, 'a', newline='') as csvfile:
                             #'a' to append instead of overwrite the first line each time
                 writer = csv.writer(csvfile)
-                writer.writerow(empty)
-        countprof += 1
+                writer.writerow(plist)
+        pcounter += 1
         
-    countdep += 1
+    dcounter += 1
     #move to next department until finished with all departments
 
